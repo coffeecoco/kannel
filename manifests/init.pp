cab308cdfee58502822b45e11dee130d2fcc24d5
coffee <coffee@10.10.10.7>
2015-02-08 11:09:10 +1100

    aa

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,25 +10,8 @@
 #
 # Sample Usage:
 #
-class kannel (
-  $kannel_admin_port = $kannel::params::kannel_admin_port,
-  $kannel_admin_pwd = $kannel::params::kannel_admin_pwd,
-  $sms_stat_pwd = $kannel::params::sms_stat_pwd,
-  $hub_listen_port = $kannel::params::hub_listen_port,
-  $hub_http_address = $kannel::params::hub_http_address,
-  $sendsms_port = $kannel::params::sendsms_port,
-  $redis_ip = $kannel::params::redis_ip,
-  $redis_port = $kannel::params::redis_port,
-  $smpp_admin_port = $kannel::params::smpp_admin_port,
-  $smpp_admin_pwd = $kannel::params::smpp_admin_pwd,
-  $smppbox_port = $kannel::params::smppbox_port,
-  $smppbox_ssl = $kannel::params::smppbox_ssl,
-  $ssl_cert_file = $kannel::params::ssl_cert_file,
-  $ssl_cert_key =  $kannel::params::ssl_cert_key,
-  $smpp_systemid = $kannel::params::smpp_systemid,
- ) inherits kannel::params {
    
-   class kannel::config(
+   class kannel (
   $kannel_admin_port = "13010",
   $kannel_admin_pwd = foobar,
   $sms_stat_pwd = foobar,
